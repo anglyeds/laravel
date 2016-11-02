bd3de86e96c0cf611f2fb4faaff0ea0b412c23b7
JEFFREY <anglyeds@gmail.com>
2016-11-02 17:53:20 +0800

    2 of november

--- system/app/Http/routes.php
+++ system/app/Http/routes.php
@@ -12,5 +12,45 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "WELCOME TO MY SITE";
+});
+
+Route::get('hello/{name}', function($name) {
+	echo 'Hello ' . $name;;
+});
+
+// Create an item
+Route::post('test', function(){
+	echo 'POSTED';
+});
+
+// Read an item
+Route::get('test', function(){
+	echo '<form action="test" method="POST">';
+	echo '<input type="submit" value="submit">';
+	echo '<input type="hidden" name="_token" value= "' . csrf_token() .'">';
+	echo '<input type="hidden" name="_method" value="delete">';
+	echo '</form>';
+});
+
+
+// Update an item
+Route::put('test', function(){
+	echo 'PUT';
+});
+
+// Delete an item
+Route::delete('test', function(){
+	echo 'DELETE';
+});
+
+Route::get('customer/{id}', function($id){
+	$customer = App\Customer::find($id);
+	echo 'Hello my name is ' . $customer->name;
+});
+
+Route::get('get_customer', function(){
+
+	$customer = App\Customer::where('name', '=', 'Tony')->first();
+	echo $customer->name;
 });
